16b3e30a88e9101db58c0549e515848df29f29b9
fundead <mullacy@gmail.com>
2013-02-21 17:06:38 +1300

    Set up sample project to throw web exceptions (for request oject)

--- raygun4py-sample/test.py
+++ raygun4py-sample/test.py
@@ -1,8 +1,8 @@
-import sys, os
+import sys, os, urllib2
 import traceback
 from provider import raygunprovider
 
-def handle_exception(exc_type, exc_value, exc_traceback):    
+def handle_exception(exc_type, exc_value, exc_traceback):
     cl = raygunprovider.RaygunSender("onPbQXtZKqJX38IuN4AQKA==")
     cl.set_version("1.2")
     print cl.send(exc_type, exc_value, exc_traceback, "myclass", ["tag1", "tag2"], {"key1": 1111, "key2": 2222})
@@ -11,7 +11,7 @@ def methodone():
     methodtwo()
 
 def methodtwo():
-    raise Exception("My exception")
+    urllib2.urlopen("gopher://nonsense.org").read()
 
 sys.excepthook = handle_exception
 
