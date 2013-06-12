095426c016944fd11e41e0068c6c929295a38a94
Martin Holman <martin308@gmail.com>
2013-06-12 14:42:29 +1200

    pypi packaging

--- README.rst
+++ README.rst
@@ -3,3 +3,4 @@ raygun4py
 =========
 
 	Raygun.io provider for Python 2.6-2.7
+	
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Raygun4py',
-    version='0.1',
+    version='0.1.2',
     packages=['provider',],
     license='LICENSE',
     long_description=open('README.rst').read(),
