991f5d667d9f9433de5f68c8d5359e3fdfa7e9db
fundead <callum@mindscape.co.nz>
2013-12-20 14:40:36 +1300

    Clarify logging sample

--- samples/sampleWithLogging.py
+++ samples/sampleWithLogging.py
@@ -8,15 +8,12 @@ rgHandler = raygunprovider.RaygunHandler("paste_your_api_key_here")
 logger.addHandler(rgHandler)
 
 def log_exception(exc_type, exc_value, exc_traceback):
-    print "Logging: %s" % exc_value
     logger.error("A python error occurred", exc_info = (exc_type, exc_value, exc_traceback))
+    print "Logging: %s" % exc_value
 
 sys.excepthook = log_exception
 
-def firstMethod():
-	secondMethod()
+def buggyMethod():
+    raise StandardError("Test exception sent via built-in handler")
 
-def secondMethod():
-	raise StandardError("Test exception sent via built-in handler")	
-
-firstMethod()
+buggyMethod()
