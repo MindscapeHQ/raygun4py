413d9304b5ff0a45e70512dadc4527843eee7b68
fundead <callum@mindscape.co.nz>
2014-03-26 14:55:56 +1300

    Add classifiers with py ver info

--- setup.py
+++ setup.py
@@ -20,5 +20,19 @@ setup(
     long_description=open('README.rst').read(),
     install_requires=[
         "jsonpickle == 0.7.0"
+    ],classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Communications',
     ],
 )
