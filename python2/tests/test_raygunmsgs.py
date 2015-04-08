d54aecd934edd90eb9670c15cf370b0264a1e7b6
fundead <callum@mindscape.co.nz>
2015-04-09 10:27:16 +1200

    Instance check

--- python2/tests/test_raygunmsgs.py
+++ python2/tests/test_raygunmsgs.py
@@ -25,11 +25,11 @@ class TestRaygunMessageBuilder(unittest.TestCase):
 
     def test_customdata(self):
         self.builder.set_customdata({1: "one"})
-        self.assertEqual(type(self.builder.raygunMessage.details['userCustomData']), type(dict))
+        self.assertTrue(isinstance(self.builder.raygunMessage.details['userCustomData'], dict))
 
     def test_tags(self):
         self.builder.set_tags([1, 2, 3])
-        self.assertEqual(type(self.builder.raygunMessage.details['tags']), type(list))
+        self.assertTrue(isinstance(self.builder.raygunMessage.details['tags'], list))
 
     def test_request_ip(self):
         self.builder.set_request_details(self.request)
