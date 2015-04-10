b6c521203909f10e6686c506920fb9723a7f039a
fundead <callum@mindscape.co.nz>
2015-04-10 16:36:29 +1200

    CLI interface

--- python2/raygun4py/cli.py
+++ python2/raygun4py/cli.py
@@ -1,4 +1,22 @@
+from optparse import OptionParser
 from raygun4py import raygunprovider
 
 def main():
-	pass
+    usage = '\n  raygun [options]\n  raygun install <apikey>\n  raygun test'
+    parser = OptionParser(usage=usage)
+    
+    parser.add_option('-a', '--apikey', dest='apikey', help='Your API key, available on the Application Settings page in the Raygun web app')
+
+    options, args = parser.parse_args()
+
+    if 'install' in args:
+        if len(args) > 1:
+            print "Installed API key! Now run 'raygun test' to check it's working"
+        else:
+            print 'Please provide a Raygun API key!'
+    elif 'test' in args:
+        print 'Sending a test exception...'
+    elif options.apikey:
+        print 'Updated API key'
+    else:
+        parser.print_help()
