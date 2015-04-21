0f555d7be60fbbcacef5bb01f178f6c228bc4f1b
fundead <callum@mindscape.co.nz>
2015-04-21 13:30:55 +1200

    WSGI middleware handler implementation

--- python2/raygun4py/middleware/wsgi.py
+++ python2/raygun4py/middleware/wsgi.py
@@ -2,31 +2,57 @@ from raygun4py import raygunprovider
 
 class Provider(object):
 
-	def __init__(self, app, apiKey):
-		self.app = app
-		self.sender = raygunprovider.RaygunSender(apiKey)
+    def __init__(self, app, apiKey):
+        self.app = app
+        self.sender = raygunprovider.RaygunSender(apiKey)
 
-	def __call__(self, environ, start_response):
-		if not self.sender:
-			print >> sys.stderr, ("Raygun-Flask: Cannot send as provider not attached")
+    def __call__(self, environ, start_response):
+        if not self.sender:
+            print >> sys.stderr, ("Raygun-Flask: Cannot send as provider not attached")
 
-		try:
-			iterable = self.app(environ, start_response)
-		except Exception as e:
-			self.sender.send_exception(exception=e) # TODO pass in request
+        try:
+            chunk = self.app(environ, start_response)
+        except Exception as e:
+            request = self.build_request(environ)
+            self.sender.send_exception(exception=e, request=request)
 
-			raise
+            raise
 
-		try:
-			for event in iterable:
-				yield event
-		except Exception as e:
-			self.sender.send_exception(exception=e)
+        try:
+            for event in chunk:
+                yield event
+        except Exception as e:
+            request = build_request(environ)
+            self.sender.send_exception(exception=e, request=request)
 
-			raise
-		finally:
-			if iterable and hasattr(iterable, 'close') and callable(iterable.close):
+            raise
+        finally:
+            if chunk and hasattr(chunk, 'close') and callable(chunk.close):
                 try:
-                    iterable.close()
+                    chunk.close()
                 except Exception as e:
-                    self.send_exception(exception=e)
+                    request = build_request(environ)
+                    self.send_exception(exception=e, request=request)
+
+    def build_request(self, environ):
+        request = {}
+
+        try:
+            request = {
+                'httpMethod': environ['REQUEST_METHOD'],
+                'url': environ['PATH_INFO'],
+                'ipAddress': environ['REMOTE_ADDR'],
+                'hostName': environ['HTTP_HOST'].replace(' ', ''),
+                'queryString': environ['QUERY_STRING'],
+                'headers': {},
+                'form': {},
+                'rawData': {}
+            }
+        except Exception:
+            pass
+
+        for key, value in environ.items():
+            if key.startswith('HTTP_'):
+               request['headers'][key] = value
+
+        return request
