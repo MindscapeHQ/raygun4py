c61692140cd50dea10236886d2143648ac400783
fundead <callum@mindscape.co.nz>
2015-05-27 10:10:30 +1200

    Update jsonpickle to 0.9.2
    Fixes encoding issue where bytes/bytestrings with high unicode code
    points (e.g 0xc2 0xa4) caused jsonpickle to throw a UnicodeDecodeError
    when pickling local or environment variables

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     description='Official Raygun provider for Python 2.6/2.7 and Python 3+',
     long_description=open('README.rst').read(),
     install_requires=[
-        'jsonpickle == 0.7.0',
+        'jsonpickle == 0.9.2',
         'blinker == 1.3.0'
     ],
     entry_points={
