67fbbe5dc5a6c1494c0e3fef11d46bd3b36482ea
Don Spaulding <donspauldingii@gmail.com>
2015-05-26 16:10:00 -0500

    PEP that 8.

--- python2/raygun4py/utilities.py
+++ python2/raygun4py/utilities.py
@@ -41,6 +41,7 @@ def camel(k):
     new_key = k.replace('_', ' ').title().replace(' ', '')
     return new_key[0].lower() + new_key[1:]
 
+
 def camelize_dict(d):
     return dict([
         (camel(k), v) for k, v in d.items()
