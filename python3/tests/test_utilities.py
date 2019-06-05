367b8dffa56a49f3718c7a90fbb9744392f4e67c
Taylor Lodge <ubermouse894@gmail.com>
2019-06-06 09:18:31 +1200

    feature(py3): support wildcards in filter_keys
    This allows you to filter environment variables with a wildcard

---
+++ python3/tests/test_utilities.py
@@ -0,0 +1,51 @@
+import unittest, sys
+import socket
+import inspect
+
+from raygun4py import utilities
+
+class TestRaygunUtilities(unittest.TestCase):
+    def test_filter_keys(self):
+        test_obj = {
+            'foo': 'bar',
+            'baz': 'qux'
+        }
+
+        utilities.filter_keys(['foo'], test_obj)
+
+        self.assertEqual(test_obj['foo'], '<filtered>')
+
+    def test_filter_keys_recursive(self):
+        test_obj = {
+            'foo': 'bar',
+            'baz': 'qux',
+            'boo': {
+                'foo': 'qux'
+            }
+        }
+
+        utilities.filter_keys(['foo'], test_obj)
+
+        self.assertEqual(test_obj['foo'], '<filtered>')
+        self.assertEqual(test_obj['boo']['foo'], '<filtered>')
+
+
+    def test_filter_keys_with_wildcard(self):
+        test_obj = {
+            'foobr': 'bar',
+            'foobz': 'baz',
+            'fooqx': 'foo',
+            'baz': 'qux'
+        }
+
+        utilities.filter_keys(['foo*'], test_obj)
+
+        self.assertEqual(test_obj['foobr'], '<filtered>')
+        self.assertEqual(test_obj['foobz'], '<filtered>')
+        self.assertEqual(test_obj['fooqx'], '<filtered>')
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
