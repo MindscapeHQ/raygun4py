b177b2797829ff282600d6d7538c2f8093d6adad
Miguel Beltran <m@beltran.work>
2025-05-21 07:51:23 +0200

    chore(deps): jsonpickle to >=4.0.4 (#117)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info[0] == 3:
     base_dir = "python3"
 
 exec(open("%s/raygun4py/version.py" % base_dir).read())
-requirements = ["jsonpickle >= 0.9.2", "blinker >= 1.3.0", "requests >= 2.9.1"]
+requirements = ["jsonpickle >= 4.0.4", "blinker >= 1.3.0", "requests >= 2.9.1"]
 
 dev_requirements = [
     "unittest2 >= 1.1.0",
