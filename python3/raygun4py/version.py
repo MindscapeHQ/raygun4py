8714c6f6b299be408427c649ae1c65439d9412ce
Kai Koenig <kai@ventego-creative.co.nz>
2026-01-12 16:52:25 +1300

    chore: Release 6.0.2

--- python3/raygun4py/version.py
+++ python3/raygun4py/version.py
@@ -2,4 +2,4 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) hatchling can read it for dynamic versioning (pyproject.toml)
 # 3) we can import it into your module
-__version__ = "6.0.1"
+__version__ = "6.0.2"
