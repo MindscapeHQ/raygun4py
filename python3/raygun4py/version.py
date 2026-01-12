f8ef25ea62fbf92a3bb8d05faab6539989fe7884
Kai Koenig <kai@ventego-creative.co.nz>
2026-01-12 15:06:47 +1300

    Commentary in version.py changes

--- python3/raygun4py/version.py
+++ python3/raygun4py/version.py
@@ -1,5 +1,5 @@
 # Store the version here so:
 # 1) we don't load dependencies by storing it in __init__.py
-# 2) we can import it in setup.py for the same reason
-# 3) we can import it into your module module
+# 2) hatchling can read it for dynamic versioning (pyproject.toml)
+# 3) we can import it into your module
 __version__ = "6.0.1"
